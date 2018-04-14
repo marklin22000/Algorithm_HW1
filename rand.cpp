4b6809a03e51497d7c605bcc1f0c7e4508a15348
Mark <marklin22000@gmail.com>
2018-04-15 01:26:47 +0800

    4

---
+++ rand.cpp
@@ -0,0 +1,26 @@
+#include <ctime>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>   /* rand(), srand(), RAND_MAX */
+#include <time.h>     /* time(), clock()           */
+using namespace std;
+long long num = pow(10,8);
+int main(int argc, char* argv[])
+{
+	long long i;
+	long long temp;
+	srand( time(NULL) );
+
+	ofstream ofs(argv[1]);
+	ofs << "num" << endl;
+	for( i = 0; i < num; i++)
+	{
+		temp = rand()*rand();
+		ofs << temp <<" " << endl;
+	}    
+	return 0;
+}
